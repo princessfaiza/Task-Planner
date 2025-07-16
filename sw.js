df5d8608c178ed5bbeb539218949e255e8ecf260
Princess Faiza <faizamomoh@gmail.com>
2025-07-16 16:31:15 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,4 +12,14 @@ self.addEventListener('fetch', (event) => {
       .then((response) => response || fetch(event.request))
   );
 });
+self.addEventListener('push', function(event) {
+  const data = event.data ? event.data.json() : {};
+  const title = data.title || 'Task Reminder';
+  const options = {
+    body: data.body || 'You have a task coming up!',
+    icon: '/icon.webp', // Make sure this exists
+    badge: '/icon.webp'
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
 
