2acf827bdcf32f40277ede57abd158268cbc7057
Princess Faiza <faizamomoh@gmail.com>
2025-07-17 09:14:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,15 @@
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
+  console.log('Service Worker: Installed');
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  self.clients.claim();
+self.addEventListener('activate', e => {
+  console.log('Service Worker: Activated');
+  return self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
-self.addEventListener('push', function(event) {
-  const data = event.data ? event.data.json() : {};
-  const title = data.title || 'Task Reminder';
-  const options = {
-    body: data.body || 'You have a task coming up!',
-    icon: '/icon.webp', // Make sure this exists
-    badge: '/icon.webp'
-  };
-  event.waitUntil(self.registration.showNotification(title, options));
-});
-
